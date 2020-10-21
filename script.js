18f01d33c80426bea3d3251a5d6d926277cdd495
Joyson Puvanasingham <60553801+enochj316@users.noreply.github.com>
2020-10-20 21:43:46 -0400

    commeneted different functions on HTML & Java

--- script.js
+++ script.js
@@ -13,23 +13,25 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+// Password Generate logic, length check 
 function generatePassword() {
   var newPassword = ""
   var possibleOptions =""
   var passwordLength = prompt("Length of Password");
   passwordLength = parseInt(passwordLength)
+  
   if (passwordLength < 8 || passwordLength > 128) 
   {
   alert("Must be over 8 or less 128 characters")
   return ""
   }
-
+// Confirming which characters and numerics to include
 var wantsLowerCase = confirm("Do you want Lowercase")
 var wantsUpperCase = confirm("Do you want Uppercase")
 var wantsNumeric = confirm("Do you want Numeric")
 var wantsSpecialCharacters = confirm("Do you want Special Characters")
 
+// Process of generating password with selected characters and numerics
 if (wantsLowerCase === true) {
 possibleOptions = possibleOptions + lowercaseLetters
 }
@@ -42,14 +44,17 @@ if (wantsNumeric === true) {
 if (wantsSpecialCharacters === true) {
       possibleOptions = possibleOptions + specialCharacters
     }
+
+    // Console log
 console.log(possibleOptions)
+console.log(passwordLength)
 
-
+// Formulating different password possibilities 
 for (let i = 0; i < passwordLength; i++) {
 newPassword += possibleOptions[Math.floor(Math.random()*possibleOptions.length)]
 }
 
-
+// New Password returned
 return newPassword
 
 }
